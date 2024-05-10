b00b2d264e3ca9ec302a92ad8ebb140f0a52f240
Janah Kuehl <janahs2@hotmail.com>
2024-05-09 22:19:44 -0300

    :sparkles: feat(app): Implementando Primefaces

--- src/main/java/br/com/adasJSF/controller/PessoaBean.java
+++ src/main/java/br/com/adasJSF/controller/PessoaBean.java
@@ -14,68 +14,64 @@ import br.com.adasJSF.model.Pessoa;
 @Named("pessoaBean")
 @SessionScoped
 
-public class PessoaBean implements Serializable{
+public class PessoaBean implements Serializable {
 	// Serial implementado pela interface
 	private static final long serialVersionUID = 1L;
-	
-	//Injeção de dependencia da classe Pessoa do pacote Model
+
+	// Injeção de dependencia da classe Pessoa do pacote Model
 	@Inject
 	private Pessoa pessoa;
-	
+
 	List<Pessoa> pessoas = new ArrayList<Pessoa>();
-	
+
 	private int idSequencia = 0;
-	
-	//Declaracao dos metodos da Bean
+
+	// Declaracao dos metodos da Bean
 	public String adicionar() {
 		pessoa.setId(this.getSequenciaId());
 		pessoas.add(pessoa);
 		pessoa = new Pessoa();
 		return "/pessoa/listar.xhtml";
 	}
-	
+
 	public String apagar(Pessoa pessoaApagar) {
 		pessoas.remove(pessoaApagar);
 		return null;
 	}
-	
+
 	public String editar() {
 		int index = pessoas.indexOf(pessoa);
 		Pessoa pessoaEdicao = pessoa;
-		
+
 		pessoas.remove(pessoa);
 		pessoas.add(index, pessoaEdicao);
-		
+
 		pessoa = new Pessoa();
-		
+
 		return "/pessoa/listar.xhtml";
 	}
-	
+
 	public String telaEdicao() {
 		return "/pessoa/editar.xhtml";
 	}
-	
+
 	public void carregarEdicao(ActionEvent event) {
-		Pessoa dadosPessoa = (Pessoa)event.getComponent().getAttributes().get("pessoa");
-		
+		Pessoa dadosPessoa = (Pessoa) event.getComponent().getAttributes().get("pessoa");
+
 		pessoa.setId(dadosPessoa.getId());
 		pessoa.setNome(dadosPessoa.getNome());
 		pessoa.setProfissao(dadosPessoa.getProfissao());
 		pessoa.setIdade(dadosPessoa.getIdade());
 		pessoa.setCidade(dadosPessoa.getCidade());
-		
-		
-	}
-	
 
-	
-	
+	}
+
 	private int getSequenciaId() {
-		//this.idSequencia++;
+		// this.idSequencia++;
 		return this.idSequencia++;
 	}
-	
-	//Getters e Setters
+
+	// Getters e Setters
 
 	public Pessoa getPessoa() {
 		return pessoa;
@@ -92,7 +88,5 @@ public class PessoaBean implements Serializable{
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
 	}
-	
-	
 
 }
